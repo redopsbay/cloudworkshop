f729fcdfe0c8d493e3af60a418988a48a3fc6702
Alfred Valderrama <alfred98valderrama@gmail.com>
2024-04-13 21:28:27 +0800

    fix(sg.tf): wrong from_port 2->22

--- lab-src/cicd/jenkins/jenkins-setup-ec2/sg.tf
+++ lab-src/cicd/jenkins/jenkins-setup-ec2/sg.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "jenkins_server" {
   }
 
   ingress {
-    from_port   = 2
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
